5280a4bc2f3813746a311c9d67200912ce9427be
Varun Teja Dunga <vdunga@evoketechnologies.com>
2019-10-18 13:54:56 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t varundunga/multi-client:latest -t varundunga/multi-client:$SHA -f ./complex/client/Dockerfile
-docker build -t varundunga/multi-server:latest -t varundunga/multi-client:$SHA -f ./complex/server/Dockerfile
-docker build -t varundunga/multi-worker:latest -t varundunga/multi-client:$SHA -f ./complex/worker/Dockerfile
+docker build -t varundunga/multi-client:latest -t varundunga/multi-client:$SHA -f ./complex/client/Dockerfile ./complex/client
+docker build -t varundunga/multi-server:latest -t varundunga/multi-client:$SHA -f ./complex/server/Dockerfile ./complex/server
+docker build -t varundunga/multi-worker:latest -t varundunga/multi-client:$SHA -f ./complex/worker/Dockerfile ./complex/worker
 docker push varundunga/multi-client
 docker push varundunga/multi-server
 docker push varundunga/multi-worker
